468342cdc65ebf33bb45a51411d3bab463525068
Bohdan <qwebeck5@gmail.com>
2022-07-03 18:23:54 +0200

    Adds support to static files

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,20 +1,23 @@
-import "p5";
 import { Environemnt } from "./environment/environment";
 import { WeatherProvider } from "./weather/weatherProvider";
 import { Hero } from "./hero/hero";
+import p5 from "p5";
 
 const hero = new Hero(Environemnt.worldSizeX / 2, Environemnt.worldSizeY * 0.9);
 const environment = new Environemnt(hero);
 const weatherProvider = new WeatherProvider();
+let heroSprite: p5.Image;
 
 function setup() {
   frameRate(10);
   createCanvas(Environemnt.worldSizeX, Environemnt.worldSizeY);
+  heroSprite = loadImage('assets/demo.png');
 }
 
 function draw() {
   background(200);
   const currentWeather = weatherProvider.getCurrentWeather();
+  image(heroSprite, 0, 0, Environemnt.worldSizeX, Environemnt.worldSizeY);
   environment.update(currentWeather);
   environment.draw();
 }
